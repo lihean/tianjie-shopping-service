ab8d8e32e019fa4eb7e1c8776d13211846271ccd
lihean <13094470553@163.com>
2021-04-20 15:40:32 +0800

    第一次代码提交

---
+++ src/main/java/com/tianjie/shopping/TianjieShoppingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tianjie.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TianjieShoppingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TianjieShoppingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tianjie/shopping/TianjieShoppingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tianjie.shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TianjieShoppingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
